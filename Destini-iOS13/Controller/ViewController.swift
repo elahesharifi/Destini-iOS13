3fff4d4ab1dcef09267159e90babf12c2fc4c839
elahe <el.sh192014@gmail.com>
2022-05-24 10:30:46 +0200

    made the StoryBrain more Scable

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,16 +14,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
-    let story0 = "You see a fork in the road."
-    let choice1 = "Take a left."
-    let choice2 = "Take a right."
+    var storyBrain = StoryBrain()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        storyLabel.text = story0
+        updateUI()
     }
 
     @IBAction func choiceMade(_ sender: UIButton) {
+        storyBrain.NextStory(userChoice: sender.currentTitle!)
+        updateUI()
+    }
+    func updateUI() {
+        
+        storyLabel.text = storyBrain.getStoryTitle()
+        choice1Button.setTitle(storyBrain.getChoices1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoices2(), for: .normal)
+
     }
     
 }
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+
+struct Story {
+    var storyTitle : String
+    var storyChoice1 : String
+    var storyChoice2 : String
+    var storyChoice1Destination: Int
+    var storyChoice2Destination: Int
+    
+    init(title:String,choice1:String, choice1Destination:Int,choice2:String, choice2Destination:Int) {
+        storyTitle = title
+        storyChoice1 = choice1
+        storyChoice2 = choice2
+        storyChoice1Destination = choice1Destination
+        storyChoice2Destination = choice2Destination
+        
+    }
+    
+}
